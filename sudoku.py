66b5c07570256478e5ad2b43507abc6253b6a6e9
Sikai Lu <74517890+dadabit@users.noreply.github.com>
2022-01-06 15:54:50 +0800

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -2,6 +2,30 @@
 # sudoku solver in python
 # created 2022.1.6 by Sikai Lu
 
+LICENSE_TEXT = '''
+MIT License
+
+Copyright (c) 2022 Sikai Lu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
 info_text = '''
 ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗    ████████╗ ██████╗ 
 ██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝    ╚══██╔══╝██╔═══██╗
@@ -100,6 +124,10 @@ def main():
 		return
 	if (sys.argv[1] == "-info"):
 		print(info_text)
+		return
+	
+	if (sys.argv[1] == "-license"):
+		print(LICENSE_TEXT)
 		return
 	
 	# logo
